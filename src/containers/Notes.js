01c73c83d09cc30b3cd78e7afeb7e8c9e725c246
KradasA4 <wichai.sw19@gmail.com>
2020-05-13 22:29:26 +0700

    add deletedNotes
    need to fix active to display as default notes, prevent delte when in deletde box

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actionTypes from '../store/constants';
+import { act } from 'react-dom/test-utils';
 
 const Notes = (props) => {
+  
   return(
     <div>
       <h1>Note-taking App</h1>
@@ -35,20 +37,29 @@ const Notes = (props) => {
       </form>
       <hr/>
 
+      {/* Notes view Section */}
       <div>
         <h2>Notes</h2>        
         <br/>
-        <button>Show active notes</button>
-        <button>Shot deleted notes</button>
+        {/* Show active notes */}
+        <button onClick={props.onSelectActive}>Show active notes</button>
+        {/* Show deleted notes */}
+        <button onClick={props.onSelectDeleted}>Shot deleted notes</button>
         <br/>
         <ul>
-          {props.activeNotes.map((note) => {
+          {
+          
+          props.currentNotes.map((note) => {
             return (
               <li>
                 {/* <span>ID: {note.id}</span><br/> */}
-                <span>Title: {note.title}</span><br/>
-                <span>Content: {note.content}</span><br/>
-                <span>Date: {note.date}</span><br/>
+                <span>Title: {note.title}</span>
+                <button onClick={() => props.onDeleteNote(note.id)}>X</button>
+                <br/>
+                <span>Content: {note.content}</span>
+                <br/>
+                <span>Date: {note.date}</span>
+                <br/>
               </li>
             )
           })}
@@ -62,14 +73,19 @@ const Notes = (props) => {
 const mapStateToProps = state => ({
     inputTitle: state.inputs.inputTitle,
     inputContent: state.inputs.inputContent,
-    activeNotes: state.notes.activeNotes
+    activeNotes: state.notes.activeNotes,
+    deletedNotes: state.notes.deletedNotes,
+    currentNotes: state.notes.currentNotes
 })
 
 const mapDispatchToProps = dispatch => {
   return {
     onTitleChange: (title) => dispatch({type: actionTypes.TITLE_CHANGE, title: title}),
     onContentChange: (content) => dispatch({type: actionTypes.CONTENT_CHANGE, content: content}),
-    onAddNote: (title, content, event) => dispatch({type: actionTypes.ADD_NOTE, title: title, content: content, event: event})
+    onAddNote: (title, content, event) => dispatch({type: actionTypes.ADD_NOTE, title: title, content: content, event: event}),
+    onDeleteNote: (id) => dispatch({type: actionTypes.DELETE_NOTE, targetId: id}),
+    onSelectActive: () => dispatch({type: actionTypes.SELECT_ACTIVE_NOTES}),
+    onSelectDeleted: () => dispatch({type: actionTypes.SELECT_DELETED_NOTES})
   }
 }
 
--- src/store/notes.js
+++ src/store/notes.js
@@ -2,7 +2,9 @@ import * as actionTypes from './constants';
 import { v4 as uuidv4 } from 'uuid';
 
 const initialState = {
-  activeNotes: []
+  activeNotes: [],
+  deletedNotes: [],
+  currentNotes: []
 }
 
 const reducer = (state=initialState, action) => {
@@ -12,16 +14,43 @@ const reducer = (state=initialState, action) => {
         action.event.preventDefault();
         return {
           ...state,
-          activeNotes: [ ...state.activeNotes,
+          activeNotes: [ 
+            ...state.activeNotes,
             {
               id: uuidv4(),
               title: action.title,
               content: action.content,
               date: new Date().toLocaleString() 
             }
-          ]
+          ],
+        }
       }
-
+    case actionTypes.DELETE_NOTE:
+      const updatedActiveNotes = state.activeNotes.filter((note) => {
+        return note.id !== action.targetId
+      })
+      const deletedNote = state.activeNotes.filter((note) => {
+        return note.id === action.targetId
+      })
+      return {
+        ...state,
+        activeNotes: updatedActiveNotes,
+        deletedNotes: [
+          ...state.deletedNotes,
+          deletedNote[0]
+        ],
+        // currentNotes: updatedActiveNotes
+      }
+    
+    case actionTypes.SELECT_ACTIVE_NOTES:
+      return {
+        ...state,
+        currentNotes: state.activeNotes,
+      }
+    case actionTypes.SELECT_DELETED_NOTES:
+      return {
+        ...state,
+        currentNotes: state.deletedNotes
       }
     default:
       return state;
