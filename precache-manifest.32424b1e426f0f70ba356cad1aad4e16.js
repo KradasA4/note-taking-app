3ea495ba44373e766ded7647f8941985a8851db2
KradasA4 <wichai.sw19@gmail.com>
2020-05-19 23:12:37 +0700

    Updates

---
+++ precache-manifest.32424b1e426f0f70ba356cad1aad4e16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1b9b24c89608a1db3fa63c88f31fe8",
+    "url": "/redux-note-taking-app/index.html"
+  },
+  {
+    "revision": "08e93e29f0c181bbde86",
+    "url": "/redux-note-taking-app/static/css/main.ae42c34c.chunk.css"
+  },
+  {
+    "revision": "ba1541f374edf8686f52",
+    "url": "/redux-note-taking-app/static/js/2.7e027a65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-note-taking-app/static/js/2.7e027a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e93e29f0c181bbde86",
+    "url": "/redux-note-taking-app/static/js/main.999aec30.chunk.js"
+  },
+  {
+    "revision": "30b71c2221a7a55d9cce",
+    "url": "/redux-note-taking-app/static/js/runtime-main.f1f131a0.js"
+  }
+]);
